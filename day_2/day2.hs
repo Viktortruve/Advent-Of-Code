f358a68548a976a5c6c82b1e21e7b0514ac0e555
Markus Pettersson <m.pettersson@tele-radio.com>
2021-12-02 11:29:36 +0100

    [day 2] Flex

--- day_2/day2.hs
+++ day_2/day2.hs
@@ -1,48 +1,7 @@
 module Day2 where
-
-type Input = [Move]
-type Result = Int
-
-data Position = MkPos { horizontal :: Int
-                      , depth      :: Int
-                      , aim        :: Int
-                      }
-
+data Position = MkPos { horizontal :: Int , depth :: Int , aim :: Int}
 data Move = Up Int | Down Int | Forward Int
-
-main :: IO ()
-main = do
-  input <- parse <$> readFile "input.txt"
-  putStrLn "DAY 2"
-  putStrLn $ "Solution 1: " ++ show (solve1 input)
-  putStrLn $ "Solution 2: " ++ show (solve2 input)
-
-parse :: String -> Input
-parse i = map parseMove $ lines i
-  where parseMove :: String -> Move
-        parseMove = (\(m:v:_) -> mkMove m (read v)) . words
-        mkMove :: String -> Int -> Move
-        mkMove "up"      = Up
-        mkMove "down"    = Down
-        mkMove "forward" = Forward
-
-solve1 :: Input -> Result
-solve1 =  (\p -> horizontal p * depth p) . foldl (flip move) (MkPos { horizontal = 0, depth = 0 })
-  where
-    updateDepth f p      = p { depth = f (depth p) }
-    updateHorizontal f p = p { horizontal = f (horizontal p) }
-    move :: Move -> Position -> Position
-    move (Up d)      = updateDepth (\x -> x - d)
-    move (Down d)    = updateDepth (+ d)
-    move (Forward h) = updateHorizontal (+ h)
-
-solve2 :: Input -> Result
-solve2 = (\p -> horizontal p * depth p) . foldl (flip move) (MkPos { horizontal = 0, depth = 0, aim = 0 })
-  where
-    updateDepth f p      = p { depth = f (depth p) (aim p) }
-    updateHorizontal f p = p { horizontal = f (horizontal p) }
-    updateAim f p        = p { aim = f (aim p) }
-    move :: Move -> Position -> Position
-    move (Up x)      = updateAim (+ x)
-    move (Down x)    = updateAim (\a -> a - x)
-    move (Forward x) = updateDepth (\d a -> d - a * x) . updateHorizontal (+ x)
+main = do input <- parse <$> readFile "input.txt"; putStrLn "DAY 2"; putStrLn $ "Solution 1: " ++ show (solve1 input); putStrLn $ "Solution 2: " ++ show (solve2 input)
+parse = let mkMove "up" = Up ; mkMove "down" = Down ; mkMove "forward" = Forward ; in map ((\(m:v:_) -> mkMove m (read v)) . words) . lines
+solve1 = let move (Up d) = \p -> p { depth = depth p - d } ; move (Down d) = \p -> p { depth = depth p + d } ; move (Forward h) = \p -> p { horizontal = horizontal p + h } ; in (\p -> horizontal p * depth p) . foldl (flip move) (MkPos { horizontal = 0, depth = 0, aim = 0 })
+solve2 = let move (Up x) = \p -> p { aim = aim p + x }; move (Down x) = \p -> p { aim = aim p - x }; move (Forward x) = (\p -> p { depth = depth p - aim p * x}) . (\p -> p { horizontal = horizontal p + x}) in (\p -> horizontal p * depth p) . foldl (flip move) (MkPos { horizontal = 0, depth = 0, aim = 0 })
