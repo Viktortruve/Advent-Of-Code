b2c3f982aa20289fcdb0517ec8f74c94a73f49f4
Markus Pettersson <m.pettersson@tele-radio.com>
2021-12-09 17:05:33 +0100

    [day 9] Add solution to both stars

--- day_9/day9.hs
+++ day_9/day9.hs
@@ -2,7 +2,7 @@ module Main where
 
 import qualified Data.Map as M
 import           Data.Char (digitToInt)
-import           Data.Tuple (swap)
+import           Data.List (sort, nub)
 import           Text.Parsec        hiding (Line, parse)
 import           Text.Parsec.String
 
@@ -16,47 +16,63 @@ main = do
 -- * Part 1
 
 solve1 :: Input -> Result
-solve1 = sum . map risklevel . lowpoints
+solve1 grid = sum . map (risklevel . peek grid) . lowpoints $ grid
   where risklevel = succ
-
-lowpoints :: Grid -> [Int]
-lowpoints = map pluckLowest . filter hasLowestPoint . areas
-  where
-    hasLowestPoint (a, b, c, d, e) = c </ [a,b,d,e]
-    pluckLowest (_,_,c,_,_) = c
-
-areas :: Grid -> [Area]
-areas grid = map mapArea coordinates
-    where
-      coordinates = M.keys grid
-      mapArea (x, y) =
-        (peek grid (x, y - 1), -- top
-         peek grid (x - 1, y), -- left
-         peek grid (x, y),     -- this point
-         peek grid (x, y + 1), -- right
-         peek grid (x + 1, y)) -- bottom
-
-(</) :: Ord a => a -> [a] -> Bool
-(</) x = not . any (<= x)
 
 peek :: Grid -> Coord -> Int
 peek = flip (M.findWithDefault wall)
   where
     wall = 10
 
+lowpoints :: Grid -> [Coord]
+lowpoints grid = map head . filter (hasLowestPoint . map (peek grid)) $ areas grid
+  where
+    hasLowestPoint points = head points </ tail points
+    (</) :: Ord a => a -> [a] -> Bool
+    (</) x = not . any (<= x)
+
+areas :: Grid -> [[Coord]]
+areas grid = map (\c -> c : neighbours grid c) coordinates
+    where
+      coordinates = M.keys grid
+
+neighbours :: Grid -> Coord -> [Coord]
+neighbours grid = filter valid . adjacents
+  where
+    valid :: Coord -> Bool
+    valid = (<= 9) . peek grid
+    adjacents (x, y) = [(x, y - 1), -- top
+                        (x - 1, y), -- left
+                        (x, y + 1), -- right
+                        (x + 1, y)] -- bottom
+
 -- * Part 2
 
 solve2 :: Input -> Result
-solve2 = undefined
+solve2 = product . take 3 . reverse . sort . map length . basins
+
+basins :: Grid -> [Basin]
+basins grid = map (explore grid) (lowpoints grid)
+
+explore :: Grid -> Coord -> Basin
+explore grid lowpoint = nub $ concatMap go (lowpoint : neighbours grid lowpoint)
+  where
+    hasNext origin next = next `notElem` [9, 10] && next > origin
+    -- | Go in one direction, and return the trace of where we went
+    go :: Coord -> [Coord]
+    go c
+      | val `elem` [9, 10]    = mempty
+      | otherwise = let nabos = filter (hasNext val . peek grid) . neighbours grid $ c
+                    in c : concatMap go nabos
+      where val = peek grid c
 
 -- * Data Types
 
 type Input   = Grid
 type Result  = Int
 
-type Area = (Int, Int, Int, Int, Int)
-
 type Coord = (Int, Int)
+type Basin = [Coord]
 type Grid = M.Map Coord Int
 
 -- * Today's parsing was actually fun and enjoyable. (no sarcasm)
@@ -67,8 +83,11 @@ parse input = either (error . show) id <$> parseFromFile parser input
 parser :: Parser Input
 parser = do
   rows <- manyTill row eof
+  -- Pair up each value with a coordinate
   pure $ M.fromList $
-    concatMap (\(index, row) -> zip (zip (repeat index) [0 ..]) row) (zip [0 ..] rows)
+   concat [[ ((ri, ci), val) | (val, ci) <- indexed row] | (row, ri) <- indexed rows]
+  where
+    indexed xs = zip xs [0 ..]
 
 row :: Parser [Int]
 row = manyTill number space
