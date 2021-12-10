91e5cba23da63a71b7e1a68de4486278bce4fc55
Markus Pettersson <m.pettersson@tele-radio.com>
2021-12-10 15:55:24 +0100

    [day 10] Golfing done for today.

--- day_10/day10.hs
+++ day_10/day10.hs
@@ -1,138 +1,59 @@
 module Main where
 
 import           Data.List     (sort)
-import           Data.Either   (rights, lefts)
+import           Data.Either   (partitionEithers)
 import           Data.Foldable (foldlM)
-import           Text.Parsec        hiding (Line, parse)
-import           Text.Parsec.String
 
 main :: IO ()
 main = do
-  input <- parse "input.txt"
+  (corrupt, incomplete) <- partitionEithers . map stack . lines <$> readFile "input.txt"
   putStrLn "DAY 10"
-  putStrLn $ "Solution 1: " ++ show (solve1 input)
-  putStrLn $ "Solution 2: " ++ show (solve2 input)
+  putStrLn $ "Solution 1: " ++ show (solve1 corrupt)
+  putStrLn $ "Solution 2: " ++ show (solve2 incomplete)
 
 -- * Part 1
 
-solve1 :: Input -> Result
-solve1 = sum . map score . lefts . map mkStack
-  where
-    score :: StackError -> Int
-    score (IllegalBrace b) =
-      case b of
-        (L, _) -> 0
-        (R, p) -> case p of
-          Soft  -> 3
-          Hard  -> 57
-          Curly -> 1197
-          Angle -> 25137
-    score _ = 0
-
-mkStack :: Line -> Either StackError (Stack Brace)
-mkStack = foldlM iter []
-  where iter :: Stack Brace -> Brace -> Either StackError (Stack Brace)
-        iter stack b =
-          case b of
-            (L, _) -> pure $ push stack b -- Put it on the pile
-            (R, _) -> do
-              case peek stack of
-                Nothing -> pure $ push stack b
-                Just top -> do -- There is some brace at the top
-                  if top == inverse b -- I.e. top: '(', b: ')' => '(' == inverse ')': True
-                    then pop' stack
-                    else Left $ IllegalBrace b -- Top character does not match incomming inverse. As such, we have failed as parents.
+solve1 :: [Char] -> Result
+solve1 = sum . map score
+  where score ')' = 3
+        score ']' = 57
+        score '}' = 1197
+        score '>' = 25137
 
 -- * Part 2
 
-solve2 :: Input -> Result
-solve2 = middle . sort . map (calcScore . autocomplete) . rights . map mkStack
-  where
-    score Soft  = 1
-    score Hard  = 2
-    score Curly = 3
-    score Angle = 4
-    calcScore = foldl (\tot b -> tot * 5 + score (snd b)) 0
-
-autocomplete :: Stack Brace -> [Brace]
-autocomplete = reverse . map inverse . reverse
-
-middle :: [a] -> a
-middle as = as !! (length as `div` 2)
+solve2 :: [[Char]] -> Result
+solve2 = middle . sort . map (calcscore . autocomplete)
+  where score ')' = 1
+        score ']' = 2
+        score '}' = 3
+        score '>' = 4
+        calcscore = foldr (\b tot -> tot * 5 + score b) 0
+        autocomplete = map inverse . reverse
+        middle as = as !! (length as `div` 2)
 
 -- * Data Types
 
-type Input   = [Line]
-type Result  = Int
+type Result = Int
 
-type Line = [Brace]
+stack :: String -> Either Char [Char]
+stack = foldlM go []
+  where go stack b
+          | open b = pure $ b : stack
+          | otherwise =
+            case stack of
+              [] -> pure [b]
+              (top:rest) -> if top == inverse b then pure rest else Left b
 
-type Brace = (Dir, Paren)
+inverse :: Char -> Char
+inverse '(' = ')'
+inverse '[' = ']'
+inverse '{' = '}'
+inverse '<' = '>'
+inverse ')' = '('
+inverse ']' = '['
+inverse '}' = '{'
+inverse '>' = '<'
 
-data Paren
-  = Soft
-  | Hard
-  | Curly
-  | Angle
-  deriving (Show, Eq)
-
-data Dir = L | R
-  deriving (Show, Eq)
-
-type Stack a = [a]
-
-data StackError
-  = EmptyStack
-  | IllegalBrace Brace
-  deriving Show
-
-pop' :: Stack a -> Either StackError (Stack a)
-pop' s = do
-  (top, rest) <- pop s
-  pure rest
-
-pop :: Stack a -> Either StackError (a, Stack a)
-pop []     = Left EmptyStack
-pop (x:xs) = pure (x, xs)
-
-peek :: Stack a -> Maybe a
-peek []     = Nothing
-peek (x:xs) = pure x
-
-push :: Stack a -> a -> Stack a
-push xs x = x : xs
-
-inverse :: Brace -> Brace
-inverse (R, p) = (L, p)
-inverse (L, p) = (R, p)
-
--- * Today's parsing was actually fun and enjoyable. (no sarcasm)
-
-parse :: FilePath -> IO Input
-parse input = either (error . show) id <$> parseFromFile parser input
-
-parser :: Parser Input
-parser = manyTill line eof
-
-line :: Parser [Brace]
-line = manyTill brace (char '\n')
-
-brace :: Parser Brace
-brace = soft <|> hard <|> curly <|> angle
-
-soft :: Parser Brace
-soft = mkBrace Soft '(' ')'
-
-hard :: Parser Brace
-hard = mkBrace Hard '[' ']'
-
-curly :: Parser Brace
-curly = mkBrace Curly '{' '}'
-
-angle :: Parser Brace
-angle = mkBrace Angle '<' '>'
-
-mkBrace :: Paren -> Char -> Char -> Parser Brace
-mkBrace b l r = do
-  d <- (char l *> pure L) <|> (char r *> pure R)
-  pure (d, b)
+open :: Char -> Bool
+open = (`elem` ['(', '[', '{', '<'])
